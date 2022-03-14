5b51c188c03713640acc61f542be2fcfa3c8c3e5
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-03-14 14:26:16 -0600

    Basic Personal Site

--- 22_03_13-00-html/src/App.js
+++ 22_03_13-00-html/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div>
+      <hr />
       <div className='h1'>THE ADVENTURES OF SHERLOCK HOLES</div>
       <br />
       <div className="h6">by</div>
       <br />
       <div className="h2">SIR ARTHUR CONAN DOYLE</div>
+      <hr />
     </div>
   )
 }
