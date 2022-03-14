089ea86d6fa9c496fcdae139d5d643111571a16b
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-03-14 00:13:22 -0600

    Basic HTML Estructure

---
+++ 22_03_13-00-html/src/App.js
@@ -0,0 +1,16 @@
+//**_______________________________________________________________________________________________________________________________________________*/
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='h1'>THE ADVENTURES OF SHERLOCK HOLES</div>
+      <br />
+      <div className="h6">by</div>
+      <br />
+      <div className="h2">SIR ARTHUR CONAN DOYLE</div>
+    </div>
+  )
+}
+
+export default App;
