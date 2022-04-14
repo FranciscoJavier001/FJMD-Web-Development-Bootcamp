0d295480375f567b9885265c207fa5034279d93f
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-04-14 10:54:45 -0500

    Slice

---
+++ 22_04_08-Scripts/Slice.js
@@ -0,0 +1,10 @@
+//**_______________________________________________________________________________________________________________________________________________*/
+
+let name = "Francisco"
+console.log(name.slice(1,3)) //** Slice nos regresa la posicion de la letra que asignamos primero y el segundo es el numero total del arreglo */
+
+let tweet = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sapiente tempora perferendis, ipsum dignissimos mollitia nesciunt"
+console.log(tweet.slice(0,100))
+
+let tweetChrome = prompt("Ingresa tu Tweet")
+alert(tweetChrome.slice(1,100))
