532fcdd70ede4a1a4eb69144720d788391ebed54
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-04-14 17:08:47 -0500

    Numbers

---
+++ 22_04_08-Scripts/numbers.js
@@ -0,0 +1,7 @@
+//**_______________________________________________________________________________________________________________________________________________*/
+
+let dogAge = prompt("Ingresa la edad de tu perro")
+
+let humanAge = ((dogAge - 2) * 4) + 21
+
+alert("La edad de tu perro es " + humanAge + " años en años humanos")
