e13593b9f11a3a9f8ef8e3d2cbf9f7e29dd20e1a
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-04-16 17:40:09 -0500

    BMI Exercise

---
+++ 22_04_08-Scripts/function.js
@@ -0,0 +1,18 @@
+//**_______________________________________________________________________________________________________________________________________________*/
+// Crear una funcion
+
+// Pero 65Kg, Altura 1.8m
+// var bmi = bmiCalculator(65, 1.8) = 65, 3.24
+
+// bmi debe de ser igual al rededor de 20 en este caso
+
+const weight = prompt("Weight")
+const height = prompt("Height")
+function bmiCalculator() {
+
+    const bmi = Math.round(weight / Math.pow(height, 2));
+    return bmi;
+}
+
+const resultado = bmiCalculator() //** Una variable que almacene el resultado */
+console.log("BMI is equal to " + resultado);
