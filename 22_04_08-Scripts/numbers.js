dcd846ff65a36f733aa19169a3a516575738e8bd
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-04-16 15:33:52 -0500

    Life Exercise

--- 22_04_08-Scripts/numbers.js
+++ 22_04_08-Scripts/numbers.js
@@ -1,7 +1,13 @@
 //**_______________________________________________________________________________________________________________________________________________*/
+// let dogAge = prompt("Ingresa la edad de tu perro")
+// let humanAge = ((dogAge - 2) * 4) + 21
+// alert("La edad de tu perro es " + humanAge + " años en años humanos")
 
-let dogAge = prompt("Ingresa la edad de tu perro")
+// function lifeInWeeks(age) { //** Declaramos la funcion y ponemos lo que recibe */
+//     var days =  (81 - age)*365; //** Operaciones */
+//     var weeks = (81 - age) * 52;
+//     var months = (81 - age) * 12;
+//     console.log("Your age is " + age + ". You have " + days + " days, " + weeks + " weeks, and " + months + " months left."); //** Consola */
+// }
 
-let humanAge = ((dogAge - 2) * 4) + 21
-
-alert("La edad de tu perro es " + humanAge + " años en años humanos")
+// lifeInWeeks(35) //** Aqui ingresamos el valor que recibe la funcion entre parentesis */
