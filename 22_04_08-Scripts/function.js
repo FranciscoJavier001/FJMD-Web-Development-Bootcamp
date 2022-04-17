e651f3c53eb3ff1b3181105eaa2820eb4e17011a
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-04-17 00:20:34 -0500

    bmiCalculator

--- 22_04_08-Scripts/function.js
+++ 22_04_08-Scripts/function.js
@@ -18,15 +18,30 @@
 // console.log("BMI is equal to " + resultado);
 
 //**_______________________________________________________________________________________________________________________________________________*/
-const name1 = prompt("Your Name")
-const name2 = prompt("Crush Name") 
+// const name1 = prompt("Your Name")
+// const name2 = prompt("Crush Name")
 
-const number = Math.random()
+// const number = Math.random()
 
-const resultado = Math.floor(number*100)+1
+// const resultado = Math.floor(number*100)+1
 
-if ( resultado >=80) {
-    alert("This is Love " + name1 + " " + name2 + " are compatible in a " + resultado + "%")
-} else {
-    alert("Is Pasion " + name1 + " " + name2 + " are compatible in a " + resultado + "%")
+// if ( resultado >=80) {
+//     alert("This is Love " + name1 + " " + name2 + " are compatible in a " + resultado + "%")
+// } else {
+//     alert("Is Pasion " + name1 + " " + name2 + " are compatible in a " + resultado + "%")
+// }
+
+//**_______________________________________________________________________________________________________________________________________________*/
+function bmiCalculator(weight, height) { //** Funcion que recibe esos parametros */
+
+  var interpretation = weight / Math.pow(height, 2); //** Instrucciones de la variable interpretation */
+
+  if (interpretation <= 18.5) {
+    return "Your BMI is " + interpretation + ", so you are underweight.";
+  } else if (interpretation > 18.5 && interpretation <= 24.9) {
+    return "Your BMI is " + interpretation + ", so you have a normal weight.";
+  } else if (interpretation > 24.9) {
+    return "Your BMI is " + interpretation + ", so you are overweight.";
+  }
+  return interpretation;
 }
