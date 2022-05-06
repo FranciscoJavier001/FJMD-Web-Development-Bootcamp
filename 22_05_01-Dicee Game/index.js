4736582a9bc02d2a76a79a60835d80b3adf47233
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-05-06 00:57:17 -0500

    Practica Finalizada

--- 22_05_01-Dicee Game/index.js
+++ 22_05_01-Dicee Game/index.js
@@ -1,27 +1,28 @@
 //**_______________________________________________________________________________________________________________________________________________*/
 
 //** Funcion para tener 2 variables con 2 numeros aleatorios */
-const num1 = Math.floor(Math.random()*6)+1 
-const num2 = Math.floor(Math.random()*6)+1 
-console.log(num1, num2); //** Ya tengo los 2 numeros aleatorios */
+num1 = Math.floor(Math.random()*6)+1
+num2 = Math.floor(Math.random()*6)+1
+// console.log(num1, num2); //** Ambos numeros funcionan */
 
-//** Mostrar ganador en el h1 */
+//** Mostrar ganador en el h1, utilizamos d.qS(Donde queramos poner el texto) y lo aparecemos con iH y el comentario que queremos poner */
 if (num1 > num2) {
-    document.querySelector("h1").innerHTML = `Ganador Jugador 1`
+    document.querySelector("h1").innerHTML = `Jugador #1 Gana`
 } else if (num1 < num2) {
-    document.querySelector("h1").innerHTML = `Ganador Jugador 2`
+    document.querySelector("h1").innerHTML = `Jugador #2 Gana`
 } else {
-    document.querySelector("h1").innerHTML = `Empate`
+    document.querySelector("h1").innerHTML = `Empate con ${num1} y ${num2}`
 }
 
 //** Ruta donde se van a guardar las nuevas imagenes */
-const num1Url = `images/dice${num1}.png`
-const num2Url = `images/dice${num2}.png`
+num1Url = `images/dice${num1}.png`
+num2Url = `images/dice${num2}.png`
+// console.log(num1Url, num2Url);
 
 //** Donde se van a mostrar las nuevas imagenes, seleccionando donde van con el querySelector, el elemento HTML y posicion */
-const newImg1 = document.querySelectorAll("img")[0]
-const newImg2 = document.querySelectorAll("img")[1]
+num1Img = document.querySelectorAll("img")[0]
+num2Img = document.querySelectorAll("img")[1]
 
-//** Elemento donde se van a mostrar */
-newImg1.setAttribute("src", num1Url)
-newImg2.setAttribute("src", num2Url)
+//** Elemento donde se van a mostrar, con sA y el elemento a mostrar */
+num1Img.setAttribute("src", num1Url)
+num2Img.setAttribute("src", num2Url)
