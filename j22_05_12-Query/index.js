619dabc184c14cbfd7ad4193fa60beed623671bf
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-05-13 21:18:34 -0500

    jQuery Manipulation

--- j22_05_12-Query/index.js
+++ j22_05_12-Query/index.js
@@ -1,2 +1,8 @@
 //**_______________________________________________________________________________________________________________________________________________*/
-$("h1").css("color", "red") //** Cambie el estilo del h1 a color rojo */
+// $("h1").css("color", "red") //** Cambie el estilo del h1 a color rojo */
+$("h1").addClass("big-title margin-50") //** Aqui no usamos el punto y puedo ingresar varias clases */
+$("h1").hasClass("margin-50") //** Asi podemos ver si tiene la clases y me regresa un booleano */
+// $("h1").removeClass("big-title") //** Asi remuevo la clase */
+
+
+$("button")
