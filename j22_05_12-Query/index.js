b661d5d8c03fe694ae8cc9698a029b0d02888783
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-05-13 22:47:30 -0500

    jQuery Attributes/Properties

--- j22_05_12-Query/index.js
+++ j22_05_12-Query/index.js
@@ -4,7 +4,12 @@
 // $("h1").hasClass("margin-50") //** Asi podemos ver si tiene la clases y me regresa un booleano */
 // $("h1").removeClass("big-title") //** Asi remuevo la clase */
 
-$("h1").text("Bye") //** Asi le cambio el texto al elemento, muestra lo que esta dentro de los "" */
+// $("h1").text("Bye") //** Asi le cambio el texto al elemento, muestra lo que esta dentro de los "" */
 
 // $("button").text("Don't Click Me")
-$("button").html("<em>Hey</em>") //** Le cambie el texto a los botones y lo puse en cursiva (solo mostro texto no etiquetas) */
+// $("button").html("<em>Hey</em>") //** Le cambie el texto a los botones y lo puse en cursiva (solo mostro texto no etiquetas) */
+
+// $("img").attr("src") //** Asi puedo ver las propiedades/atributos de la img */
+// $("a").attr("href", "https://www.yahoo.com") //** Asi le puedo cambiar el hypervinculo */
+
+// $("h1").attr("class") //** Asi puedo ver los atributos/propiedades de la clase h1 */
