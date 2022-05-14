2910353edaf4d21394a1e91bfb7ffcd72f813041
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-05-13 22:37:48 -0500

    jQuery Manipulation

--- j22_05_12-Query/index.js
+++ j22_05_12-Query/index.js
@@ -1,8 +1,10 @@
 //**_______________________________________________________________________________________________________________________________________________*/
 // $("h1").css("color", "red") //** Cambie el estilo del h1 a color rojo */
-$("h1").addClass("big-title margin-50") //** Aqui no usamos el punto y puedo ingresar varias clases */
-$("h1").hasClass("margin-50") //** Asi podemos ver si tiene la clases y me regresa un booleano */
+// $("h1").addClass("big-title margin-50") //** Aqui no usamos el punto y puedo ingresar varias clases */
+// $("h1").hasClass("margin-50") //** Asi podemos ver si tiene la clases y me regresa un booleano */
 // $("h1").removeClass("big-title") //** Asi remuevo la clase */
 
+$("h1").text("Bye") //** Asi le cambio el texto al elemento, muestra lo que esta dentro de los "" */
 
-$("button")
+// $("button").text("Don't Click Me")
+$("button").html("<em>Hey</em>") //** Le cambie el texto a los botones y lo puse en cursiva (solo mostro texto no etiquetas) */
