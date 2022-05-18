28a22a6b45f6b24d1bb51d72ab0f261bf406ed09
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-05-18 00:49:39 -0500

    FS Functions

--- 22_05_17-Intro To Node/index.js
+++ 22_05_17-Intro To Node/index.js
@@ -1,1 +1,6 @@
 //**_______________________________________________________________________________________________________________________________________________*/
+
+const fs = require("fs")
+
+//** Aunque le cambie algo a file2 el que va a remplazar el texto es el file1 que manda, osea el source */
+fs.copyFileSync("file1.txt", "file2.txt") //** Asi puedo crear otro archivo, el primero es el src y el segundo el destino */
