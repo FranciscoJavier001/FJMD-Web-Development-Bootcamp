6eae931f3c1f7cd7e86e9ae4622c73b5f8313dfc
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-05-19 05:48:04 -0500

    Utilizacion de Nodemon

--- 22_05_19-My Express Server/server.js
+++ 22_05_19-My Express Server/server.js
@@ -1,10 +1,19 @@
 //**_______________________________________________________________________________________________________________________________________________*/
+//** Para inicializar nodemon, le ponemos "nodemon server.js (osea nodemon y el archivo a estar refrescando para cambios */
 const express = require('express') //** Creacion de variable donde solicitamos express desde el package */
 const app = express() //** app va a ser una funcion de express */
 
-app.get("/", function(req, res){ //** En la bara / (que es el main), hago una funcion que recibe el req, res */
+app.get("/", function(req, res) { //** En la bara / (que es el main), hago una funcion que recibe el req, res */
     // console.log(request); //** Muestro el req */
-    res.send("<h1>Hello World</h1>") //** send(), es un metodo; Quiero que me responsas en el html con algo */
+    res.send("<h1>Hello World!</h1>") //** send(), es un metodo; Quiero que me responsas en el html con algo */
+})
+
+app.get("/contact", function(req, res) {
+    res.send("<h2>Contact me at: user.streaming.001@gmail.com</h2>")
+})
+
+app.get("/about", function(req, res) {
+    res.send("<h2>I'm Francisco</h2>")
 })
 
 app.listen(3000, function(){ //** Voy a poner el puerto donde quiero escuchar a express, defino una funcion anonima */
