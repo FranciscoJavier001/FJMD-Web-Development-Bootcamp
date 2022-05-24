d4c6a6b8b69f6f354bf0318886deffd4e70c8154
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-05-23 23:49:09 -0500

    Documentacion

--- 22_05_21-Weather Proyect/app.js
+++ 22_05_21-Weather Proyect/app.js
@@ -5,23 +5,23 @@ const bodyParser = require("body-parser")
 
 const app = express() //** La variable app es una funcion de express */
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true})) //** Es para poder recibir parametros, desde el body */
 
 app.get("/", function(req, res) { //** app.get en el root del path, va a recibir de una funcion un req y una res(respuesta) */
 
     res.sendFile(__dirname + "/index.html") //** La respuesta de envia desde direccion URL(__dirname) y el path de donde recibo */
 })
 
-app.post("/", function(req, res) {
+app.post("/", function(req, res) { //** Vamos a hacer una peticion, que me trae unos req y una respuesta, que son estas */
     // console.log(req.body.cityName)
     // console.log("Post request recived");
 
     //** Voy a meter esas variables en unas nuevas y solo las pongo en el array */
-const query = req.body.cityName
-const apiKey = "3848c71c0ab3b7559459ead604c7f5fb"
-const unit = "metric"
+const query = req.body.cityName //** El query en el URL va a ser la variable de cityName */
+const apiKey = "3848c71c0ab3b7559459ead604c7f5fb" //** Esta la tenemos nosotros podefault */
+const unit = "metric" //** Esta igual la tenemos que agregar, o aqui poner */
 //** Meto el url en una variable */
-const url = (`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit}&appid=${apiKey}`)
+const url = (`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit}&appid=${apiKey}`) //** Url con parametros definidos arriba */
 
 https.get(url, function(response) { //** El metodo que va a tener el https, que nos trae una funcion con una respuesta */
     // console.log(response); //** Asi sale el response en la consola, navega hasta el statusCode:200, statusMessage: 'OK' */
@@ -37,11 +37,13 @@ https.get(url, function(response) { //** El metodo que va a tener el https, que 
         // console.log(weatherDescription); //** Lo mostre en consola */
         // console.log(weatherData.name); //** Para ponerlo bonito abajo */
         //** Asi lo mostramos, solo un send, puedo pasarle etiquetas html, pero lo brinco con el write */
+
+        //** Esto lo vamos a mostrar en el body */
         res.write(`<h1>The temperature in ${weatherData.name} is ${weatherData.main.temp} degress Celcius</h1>`) 
         res.write(`<p>The Weather is currently ${weatherData.weather[0].description}</p>`) 
         const iconUrl = (`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`) //** Creamos una variable del URL */
-        res.write(`<img src="${iconUrl}">`) //** Asi mostramos la variable */
-        res.send()
+        res.write(`<img src="${iconUrl}">`) //** Asi mostramos la variable de la imagen */
+        res.send() //** Para que se mande, o creo que es para que se dibuje */
         
         // const object = { //** Creamos una variable que va a ser un objeto */
         //     name: "Francisco", //** Estas son sus propiedades */
