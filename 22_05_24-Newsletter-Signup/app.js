362e5e33bfcbe5687cc39c735065bbd322b260c8
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-05-31 17:37:45 -0500

    Formulario Funcionando

--- 22_05_24-Newsletter-Signup/app.js
+++ 22_05_24-Newsletter-Signup/app.js
@@ -1,39 +1,23 @@
 //**_______________________________________________________________________________________________________________________________________________*/
 const express = require("express")
-const https = require("https")
 const bodyParser = require("body-parser")
 const request = require("request")
 
 const app = express() //** Creamos la variable app que llama la funcion de express */
 
 app.use(express.static("public")); //** Utilizo esta ruta, para poder poner recursos a las paginas, por ejemplo el .png del signup.tml */
+app.use(bodyParser.urlencoded({extended: true})); //** Utilizo esta funcion para poder recibir los datos del formulario */
 
-app.get("/", function(req, res) {
-
-    const url = (`https://v2.jokeapi.dev/joke/Any`)
-
-    https.get(url, function(response) {
-        // console.log(response); //** Este que es completo, debo traerlo a full */
-        // console.log(`StatusCode: ` + response.statusCode);
-        // console.log(`StatusMessage: ` + response.statusMessage);
-
-        response.on("data", function(data) {
-            // console.log(data);
-            const jokeData = JSON.parse(data)
-            // console.log(jokeData);
-
-            const iconUrl = 
-
-            res.write(`<h1>JokeApi =)</h1>`)
-            res.write(`<h2>Chiste: </h2><h3>${jokeData.joke}</h3>`)
-            console.log(jokeData.joke);
-            res.send()
-        })
-    })
+app.get("/", function(req, res) { //** app.get en el root del path, va a recibir de una funcion un req y una res(respuesta) */
+    res.sendFile(__dirname + "/signup.html") //** La respuesta de envia desde direccion URL(__dirname) y el path de donde recibo, sin extencion */
 })
 
-app.get("/signup", function(req, res) { //** app.get en el root del path, va a recibir de una funcion un req y una res(respuesta) */
-    res.sendFile(__dirname + "/signup.html") //** La respuesta de envia desde direccion URL(__dirname) y el path de donde recibo */
+app.post("/", function(req, res) {
+    let firstName = req.body.fName; //** Recibo el valor del input con el nombre fName */
+    let lastName = req.body.lName; //** Recibo el valor del input con el nombre lName */
+    let email = req.body.email; //** Recibo el valor del input con el nombre email */
+
+    // console.log(firstName, lastName, email); //** Imprimo los valores de los inputs */
 })
 
 app.listen(3000, function() { //** Para escuchar las peticiones en el puerto 3000, que se ejecute la funcion anonima y que haga lo siguiente */
