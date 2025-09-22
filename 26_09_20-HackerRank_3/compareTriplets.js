e33e580f9bd150172273ac85f5be5d1d22b96281
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2025-09-21 19:26:18 -0600

    Fail Test / Solved

--- 26_09_20-HackerRank_3/compareTriplets.js
+++ 26_09_20-HackerRank_3/compareTriplets.js
@@ -14,19 +14,51 @@
 /* Definimos un funcion que inicialice una funcion y le pasamos los parametros de ambos objetos */
 /* Documento consigue un elemento pot id, por el id inserta el texto, variable definida junta con comillas */
 
-const twoParameter = (a,b) => {
+// const twoParameter = (a,b) => {
+//     let sub1 = 0
+//     let sub2 = 0
+// }
+
+// for(i=0; i<3; i++){
+//     if (sub1>sub2) sub1++;
+//     else if (sub1<sub2) sub2++
+// }
+
+// return su1, sub2
+
+// const main = twoParameter(1,2,3)(1,2,3)
+// document.getElementById('output').innerText.join(' ')
+
+
+
+function twoParameter(a,b){
     let sub1 = 0
     let sub2 = 0
+
+    for (let i=0; i<3; i++){
+        if(a[i] > b[i]) sub1++
+        else if (a[i] < b[i]) sub2++ 
+    }
+    return [sub1, sub2]
 }
 
-for(i=0; i<3; i++){
-    if (sub1>sub2) sub1++;
-    else if (sub1<sub2) sub2++
-}
+const main = twoParameter([5,6,7], [5,6,10])
+document.getElementById('output').innerText = main.join('')
 
-return su1, sub2
 
-const main = twoParameter(1,2,3)(1,2,3)
-document.getElementById('output').innerText.join(' ')
+// function compareTriplets(a, b) { /* Creamos una funcion que reciba 2 parametros */
+//   let alice = 0; /* Definimos una variable que cambie let */
+//   let bob = 0; /* Definimos otra variable que cambie let */
 
+//   for (let i = 0; i < 3; i++) { /* Iteramos */
+//     if (a[i] > b[i]) alice++; /* Si el parametro a en la iteracion i es mayor que b en la iteracion en i suma puntoa alice */
+//     else if (a[i] < b[i]) bob++; /* Pero si la iteracion de a en i es menor que la iteracion de b en i entonces suma puntos a bob */
+//   }
+
+//   return [alice, bob]; /* Mostramos el resultado */
+// }
+
+// // ejemplo directo en navegador
+// const result = compareTriplets([5, 6, 7], [3, 6, 10]); 
+// document.getElementById('output').innerText = result.join(' ');
 
