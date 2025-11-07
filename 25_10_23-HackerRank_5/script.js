6986824e9d3f781f65b87c6e7d31430181dd2cd0
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2025-11-06 20:05:17 -0600

    Test

--- 25_10_23-HackerRank_5/script.js
+++ 25_10_23-HackerRank_5/script.js
@@ -73,3 +73,46 @@ El Promedio de los positivos son los numeros positivos, entre todos los numeros,
 
 Muestra sus resultados en consola
 */
+
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    // Write your code here
+
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
