3e25ed5ad81769b0076765ef0d37a719d1577bdf
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2025-11-24 19:34:47 -0600

    New Anotations

--- 25_11_14-HackerRank_7/script.js
+++ 25_11_14-HackerRank_7/script.js
@@ -18,3 +18,43 @@ function miniMaxSum(arr) { /* Definimos una funcion, que reciba un argumento */
     
     console.log(minSum + " " + maxSum); /* Mostrmos en consola el minumosum + el espacio y luego la suma maxima */
 }
+
+//* /*  */ *//*  */ *//* /* /* /*  */ *//*  */ *//* /*  */ *//*  */ *//* /* /*  */ *//*  */ *//* /*  */ *//*  */ */ */ */ */ */ */ */ */ */ */ */ */ */ */ */ */ */
+
+function miniMaxSum(arr) { 
+    /* Definimos una función que recibe un arreglo de 5 enteros. */
+
+    let total = 0;  
+    /* Inicializamos una variable acumuladora (sumará todos los valores del arreglo). */
+
+    let min = arr[0];  
+    /* Inicializamos el valor mínimo con el primer elemento del arreglo. */
+
+    let max = arr[0];  
+    /* Inicializamos el valor máximo con el primer elemento del arreglo. */
+    
+    for (let i = 0; i < arr.length; i++) {  
+        /* Recorremos el arreglo desde la posición 0 hasta arr.length - 1. */
+
+        const value = arr[i];  
+        /* Guardamos en 'value' el valor actual del arreglo en la posición i. */
+
+        total += value;  
+        /* Sumamos el valor actual al acumulador total. */
+        
+        if (value < min) min = value;  
+        /* Si el valor actual es menor que el mínimo registrado, actualizamos 'min'. */
+
+        if (value > max) max = value;  
+        /* Si el valor actual es mayor que el máximo registrado, actualizamos 'max'. */
+    }
+    
+    const minSum = total - max; 
+    /* Para obtener la suma mínima, restamos el valor máximo a la suma total. */
+
+    const maxSum = total - min; 
+    /* Para obtener la suma máxima, restamos el valor mínimo a la suma total. */
+    
+    console.log(minSum + " " + maxSum);  
+    /* Imprimimos ambas sumas en formato requerido: "minSum maxSum". */
+}
