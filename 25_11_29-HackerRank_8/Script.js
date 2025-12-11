155f88455078a0776ea9043fb3d1ad738a5d08ec
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2025-12-10 18:49:34 -0600

    HackerRank Solved VelitasCumpleañosAltas

--- 25_11_29-HackerRank_8/Script.js
+++ 25_11_29-HackerRank_8/Script.js
@@ -1,1 +1,28 @@
-alert("miedo")
+function birthdayCakeCandles(candles) {
+  let max = 0;
+  let count = 0;
+
+  for (let h of candles) {
+    if (h > max) {
+      max = h;
+      count = 1;
+    } else if (h === max) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+// Función para conectar con el HTML
+function runTest() {
+  const inputValue = document.getElementById("inputCandles").value;
+
+  // Convertir texto a array de números
+  const candles = inputValue.trim().split(" ").map(Number);
+
+  const result = birthdayCakeCandles(candles);
+
+  document.getElementById("output").textContent =
+    `Velas más altas: ${result}`;
+}
